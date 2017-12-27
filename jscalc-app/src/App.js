de60da4ff980521574d034b5a3e5e480de7d8a07
James Li <james.li@opower.com>
2017-12-26 21:30:43 -0800

    Adding arithmetic images

--- jscalc-app/src/App.js
+++ jscalc-app/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Plus from './Plus';
+import Minus from './Minus';
+import Multiply from './Multiply';
+import Divide from './Divide';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +12,13 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">JS Calculator</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Plus/>
+          <Minus/>
+          <Multiply/>
+          <Divide/>
         </p>
       </div>
     );
